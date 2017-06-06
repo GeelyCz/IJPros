7a9276193256f873fbedddfbcc222d2ccceeff62
GeelyCz <Zhang.Chen1@geely.com>
2017-06-06 09:17:11 +0800

    Update TestGit.java
    add response method

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -5,4 +5,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("你好 IDEA！");
     }
+    public static void response(String name){
+        System.out.println("你也好呀~");
+    }
+
 }
